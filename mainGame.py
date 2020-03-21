ccf98f99c101f9d9188e441bab467636a5c427d7
kimetix99 <qpico99@gmail.com>
2020-03-21 21:16:24 +0100

    music changes added to the game

--- mainGame.py
+++ mainGame.py
@@ -23,16 +23,24 @@ class Game():
                                   "img/pizza.png", "img/wallet.png"]
         self.spanish_enemy_types = ["img/fork.png",
                                   "img/knife.png", "img/wallet.png"]
+        self.china_music = "music/chinese_level.mp3"
+        self.italia_music = "music/italian_level.mp3"
+        self.spain_music = "music/spanish_level.mp3"
+        self.final_boss_music = "music/final_bos.mp3"
+
         self.isGameFinished = False
         
 
     def run(self):
-        self.level1 = Level(self.ui, self.china_enemy_types, 7)
+        self.level1 = Level(self.ui, self.china_enemy_types, 7, self.china_music)
+        self.ui.set_sound(self.china_music)
         if self.level1.run():
+            self.ui.set_sound(self.italia_music)
             self.ui.set_background('img/backgrounds/italy.png')
-            self.level2 = Level(self.ui, self.italy_enemy_types, 12)
+            self.level2 = Level(self.ui, self.italy_enemy_types, 12, self.italia_music)
             if self.level2.run():
+                self.ui.set_sound(self.spain_music)
                 self.ui.set_background('img/backgrounds/spain.jpg')
-                self.level3 = Level(self.ui, self.spanish_enemy_types, 17, final_level=True)
+                self.level3 = Level(self.ui, self.spanish_enemy_types, 17, self.spain_music, final_level=True)
                 self.level3.run()
         self.isGameFinished = True
